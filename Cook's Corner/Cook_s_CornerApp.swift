d9dcf6dc6bfb14647457aa55d52b3ecafc168e62
KhuePM <minhkhuepham99@gmail.com>
2024-12-11 21:16:14 +0700

    Initial Commit

---
+++ Cook's Corner/Cook_s_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cook_s_CornerApp.swift
+//  Cook's Corner
+//
+//  Created by KhuePM on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Cook_s_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
